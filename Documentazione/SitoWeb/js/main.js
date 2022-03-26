1c47ab4a7c0cee8b1442601f0d17416cb6dce52a
Palarvind03 <technical.stu03@gmail.com>
2022-03-26 02:06:26 +0100

    sitoWeb: creato la sezione: game story(home)

--- Documentazione/SitoWeb/js/main.js
+++ Documentazione/SitoWeb/js/main.js
@@ -1,5 +1,16 @@
 let logo = document.querySelector(".logo");
-
 logo.addEventListener("click", () => {
   location.href = "./index.html";
+});
+
+let menuBtn = document.querySelector(".menuInput");
+let overlayMenu = document.querySelector(".overlayMenu");
+menuBtn.addEventListener("click", () => {
+  if (menuBtn.checked) {
+    overlayMenu.style.display = "block";
+    document.body.style.overflow = "hidden";
+  } else {
+    overlayMenu.style.display = "none";
+    document.body.style.overflow = "scroll";
+  }
 });
