9d4b4334108f693c4fbd4c0c18930c25e60de45e
Mattia Barbero <58552421+MattiaBarbe@users.noreply.github.com>
2022-05-26 09:12:51 +0200

    python installer and boot

---
+++ Game_Installer/Python_Installation.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Checking package install..."
+sleep 2
+var='False'
+type -P python3 >/dev/null 2>&1 && var='True'
+if [[ $var == 'False' ]]
+then
+    echo "Python mancante..."
+    sleep 2
+    sudo apt-get update
+    sudo apt-get install python3.6
+fi
+var='False'
+type -P pip >/dev/null 2>&1 && var='True'
+if [ $var == 'False' ]
+then
+echo "2"
+    sudo apt install python3-pip
+fi
+var='False'
+
+if python3 -c 'import pkgutil; exit(not pkgutil.find_loader("pygame"))'; then     var='True'; else     var='False'; fi
+
+if [ $var == 'False' ]
+then
+    sudo pip install pygame
+    var='True'
+fi
+
+if [ $var == 'True' ]
+then
+    echo "All packages are succefly installed..."
+    sleep 1
+fi
+
+echo -n "Vuoi avviare il gioco(s/n): "
+read avvia
+
+if [ $avvia == 's' ]
+then
+cd ..
+cd ./Python
+python3 main.py
+fi
