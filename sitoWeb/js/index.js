f423ca0e1183923f9db237ecda763bd992ea7fb6
Palarvind03 <technical.stu03@gmail.com>
2022-05-27 23:30:03 +0100

    sitoWeb: aggiornato la grafica

--- sitoWeb/js/index.js
+++ sitoWeb/js/index.js
@@ -1,20 +1,116 @@
 /*************** sezione 1(main) ***************/
 bgSez1 = document.querySelector(".mainContenitoreSez1");
 
-const createMovingBg = () => {
-  mvBg = document.createElement("div");
-  mvBg.setAttribute("class", "movingBg");
-  bgSez1.appendChild(mvBg);
-};
-
-setInterval(async function () {
-  createMovingBg();
-  tlAnimaBg = gsap.to(".movingBg", {
-    x: bgSez1.offsetWidth,
-    duration: 5,
-    ease: "none",
-  });
-}, Math.floor(Math.random() * 3000) + 5000);
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#565656",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.3,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "top",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: false,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 250,
+        size: 0,
+        duration: 2,
+        opacity: 0,
+        speed: 3,
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
 
 /*************** sezione 2 ***************/
 mainTestoSez2 = document.querySelector(".testoSez2");
@@ -58,12 +154,12 @@ for (let i = 0; i < pagBtns.length; i++) {
 
     tlAnimaTestoSez2
       .set(".testoSez2", { opacity: 1 })
-      .to(".testoSez2", { opacity: 0, duration: 0.8 })
+      .to(".testoSez2", { opacity: 0, duration: 0.5 })
       .then(() => {
         mainTestoSez2.innerHTML = contenutoPagineSez2[i];
       })
       .then(() => {
-        tlAnimaTestoSez2.to(".testoSez2", { opacity: 1, duration: 0.8 });
+        tlAnimaTestoSez2.to(".testoSez2", { opacity: 1, duration: 0.5 });
       });
 
     //for per controllare bottone pagina cliccato e per cambiare l'opacita
@@ -77,15 +173,15 @@ for (let i = 0; i < pagBtns.length; i++) {
   });
 }
 
-/*************** sezione 3s ***************/
+/*************** sezione 3 ***************/
 gsap.to(".box", {
   duration: 1,
   scale: 0.1,
   y: 40,
   ease: "power1.inOut",
   stagger: {
-    grid: [3,6],
+    grid: [3, 6],
     from: "center",
-    amount: 1.5
-  }
+    amount: 1.5,
+  },
 });
--- sitoWeb/js/main.js
+++ sitoWeb/js/main.js
@@ -1,3 +1,20 @@
+const showLoadingScreen = () => {
+  let loadingScreen = document.querySelector(".loaderScreen");
+  loadingScreen.style.display = "block";
+  document.body.style.overflow = "hidden";
+
+  setTimeout(() => {
+    gsap
+      .fromTo(".loaderScreen", { opacity: 1 }, { opacity: 0, duration: 0.5 })
+      .then(() => {
+        loadingScreen.style.display = "none";
+      });
+
+    document.body.style.overflow = "visible";
+  }, Math.floor(Math.random() * 2500) + 1000);
+};
+showLoadingScreen();
+
 let logo = document.querySelector(".logo");
 logo.addEventListener("click", () => {
   location.href = "./index.html";
@@ -14,3 +31,76 @@ menuBtn.addEventListener("click", () => {
     document.body.style.overflow = "scroll";
   }
 });
+
+if (sessionStorage.getItem("currentPage") == undefined) {
+  sessionStorage.setItem("currentPage", "home");
+}
+
+let tabsDot = [];
+let tabs = document.querySelectorAll(".tabs span");
+let menuTabs = document.querySelectorAll(".tabsOverlayMenu span a");
+
+for (let i = 0; i < tabs.length; i++) {
+  tabsDot.push(window.getComputedStyle(tabs[i], ":after"));
+
+  tabs[i].addEventListener("click", (e) => {
+    if (e.target.innerText == "home") {
+      sessionStorage.setItem("currentPage", "home");
+    }
+    if (e.target.innerText == "about us") {
+      sessionStorage.setItem("currentPage", "aboutUs");
+    }
+    if (e.target.innerText == "contacts") {
+      sessionStorage.setItem("currentPage", "contacts");
+    }
+    showLoadingScreen();
+  });
+
+  menuTabs[i].addEventListener("click", (e) => {
+    if (e.target.innerText == "home") {
+      sessionStorage.setItem("currentPage", "home");
+    }
+    if (e.target.innerText == "about us") {
+      sessionStorage.setItem("currentPage", "aboutUs");
+    }
+    if (e.target.innerText == "contacts") {
+      sessionStorage.setItem("currentPage", "contacts");
+    }
+  });
+}
+
+if (sessionStorage.getItem("currentPage") === "home") {
+  document.documentElement.style.setProperty("--displayHomeDot", "block");
+  document.documentElement.style.setProperty("--displayAboutUsDot", "none");
+  document.documentElement.style.setProperty("--displayContactsDot", "none");
+
+  menuTabs[0].style.color = "#e5b62e";
+  menuTabs[1].style.color = "#333333";
+  menuTabs[2].style.color = "#333333";
+}
+
+if (sessionStorage.getItem("currentPage") === "aboutUs") {
+  document.documentElement.style.setProperty("--displayHomeDot", "none");
+  document.documentElement.style.setProperty("--displayAboutUsDot", "block");
+  document.documentElement.style.setProperty("--displayContactsDot", "none");
+
+  menuTabs[0].style.color = "#333333";
+  menuTabs[1].style.color = "#e5b62e";
+  menuTabs[2].style.color = "#333333";
+}
+
+if (sessionStorage.getItem("currentPage") === "contacts") {
+  document.documentElement.style.setProperty("--displayHomeDot", "none");
+  document.documentElement.style.setProperty("--displayAboutUsDot", "none");
+  document.documentElement.style.setProperty("--displayContactsDot", "block");
+
+  menuTabs[0].style.color = "#333333";
+  menuTabs[1].style.color = "#333333";
+  menuTabs[2].style.color = "#e5b62e";
+}
+
+let copyrightContenitore = document.querySelector(".contenitoreCopyright");
+const d = new Date();
+
+copyrightContenitore.innerText =
+  "© " + d.getFullYear() + " Copyright: KikisKey.it";
